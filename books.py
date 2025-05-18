3f5bba3f63925ca42a1bb02650f1b7faf9ce5ef2
Sathya <neelasathya06@gmail.com>
2025-05-18 18:43:49 +0530

    parameters implemented for restAPI

--- books.py
+++ books.py
@@ -2,6 +2,28 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/api-endpoint")
-async def first_api():
-    return {"message": "Hello Sathya!"}
+BOOKS = [
+    {'title': 'Title One', 'author': 'Author One', 'category': 'science'},
+{'title': 'Title Two', 'author': 'Author Two', 'category': 'Math'},
+{'title': 'Title Three', 'author': 'Author Three', 'category': 'History'},
+{'title': 'Title Four', 'author': 'Author Four', 'category': 'Engineering'},
+{'title': 'Title Five', 'author': 'Author Five', 'category': 'Engineering'},
+{'title': 'Title Six', 'author': 'Author Six', 'category': 'History'}
+]
+
+@app.get("/books")
+async def read_all_books():
+    return BOOKS
+
+# @app.get("/books/{dynamic_param}")
+# async def read_all_books(dynamic_param: str):
+#     return {'dynamic_param': dynamic_param}
+
+
+@app.get("/books/{book_title}")
+async def read_all_books(book_title: str):
+    for book in BOOKS:
+        if book.get('title').casefold() == book_title.casefold():
+            return book
+        return None
+    return None
