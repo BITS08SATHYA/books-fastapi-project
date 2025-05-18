972a507d667e3d5461e426e651147b9479ea72e3
Sathya <neelasathya06@gmail.com>
2025-05-18 18:54:29 +0530

    query parameters implemented for restAPI

--- books.py
+++ books.py
@@ -27,3 +27,22 @@ async def read_all_books(book_title: str):
             return book
         return None
     return None
+
+
+@app.get("/books/")
+async def read_category_by_query(category: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get('category').casefold() == category.casefold():
+            books_to_return.append(book)
+    return books_to_return
+
+
+@app.get("/books/{book_author}/")
+async def read_author_by_query(book_author: str, category: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get('author').casefold() == book_author.casefold() and \
+            book.get('category').casefold() == category.casefold():
+            books_to_return.append(book)
+    return books_to_return
