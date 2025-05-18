9461001626e7321d90cb0f59ef8f3c69ae908e63
Sathya <neelasathya06@gmail.com>
2025-05-18 19:11:20 +0530

    put api implemented for restAPI

--- books.py
+++ books.py
@@ -51,3 +51,9 @@ async def read_author_by_query(book_author: str, category: str):
 @app.post("/books/create_book")
 async def create_a_book(new_book=Body()):
     BOOKS.append(new_book)
+
+@app.put("/books/update_book")
+async def update_book(update_book=Body()):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get('title').casefold() == update_book.get('title').casefold():
+            BOOKS[i] = update_book
